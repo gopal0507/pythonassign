ad941665046d7efa857cd39ea29383b89d602680
gopal0507 <gopalcbabu@gmail.com>
2018-10-31 15:48:24 +0530

    first commit

---
+++ data.py
@@ -0,0 +1,22 @@
+def Faculties():
+    faculties=[
+        {
+            'dept':'Mechanical Engineering',
+            'name':'Sam C B',
+            'mob':'8086485167',
+            'email':'sam007@gmail.com'
+        },
+         {
+            'dept':'Electrical Engineering',
+            'name':'Raghunathan C',
+            'mob':'8086485345',
+            'email':'raghu@gmail.com'
+        },
+         {
+            'dept':'Civil Engineering',
+            'name':'Asha Xavier',
+            'mob':'93888485167',
+            'email':'ashaxavier@gmail.com'
+        }
+    ]
+    return faculties
